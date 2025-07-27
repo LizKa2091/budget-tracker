6ed1d757a946a2a200a3ff26ce485a3e2991fd58
lizka <lizka4231@gmail.com>
2025-07-28 01:22:38 +0300

    feat(use import pdf): implement extact exp from json function

--- frontend/src/features/import-pdf/model/useImportPdf.ts
+++ frontend/src/features/import-pdf/model/useImportPdf.ts
@@ -1,18 +1,25 @@
 import { useMutation } from "@tanstack/react-query";
+import { extractExpFromJson } from "../../../shared/lib/extractExpFromJson";
+import { type IExpenseItem } from "../../../shared/types/expenses";
 
-const importPdf = async (pdfFile: File): Promise<unknown> => {
+const importPdf = async (pdfFile: File): Promise<IExpenseItem[]> => {
    const response = await fetch('http://localhost:4000/upload', {
       method: 'POST',
+      headers: {
+         'Content-Type': 'application/json'
+      },
       body: pdfFile
    });
 
    if (!response.ok) throw new Error('ошибка сервера');
+
+   const result = await response.json();
    
-   return await response.json();
+   return extractExpFromJson(result);
 };
 
 export const useImportPdf = () => {
-   return useMutation({
+   return useMutation<IExpenseItem[], Error, File>({
       mutationKey: ['importPdf'],
       mutationFn: importPdf,
       retry: 1
