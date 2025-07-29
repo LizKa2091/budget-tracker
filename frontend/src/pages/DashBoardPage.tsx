44c1bda49d91e1ddc022511f25d727cd113bbfd1
lizka <lizka4231@gmail.com>
2025-07-29 12:30:13 +0300

    feat(budget chart): add displaying chart by id

--- frontend/src/pages/DashBoardPage.tsx
+++ frontend/src/pages/DashBoardPage.tsx
@@ -10,7 +10,7 @@ const DashBoardPage: FC = () => {
    }
    return (
       <>
-         <BudgetChart />
+         <BudgetChart chartId={+paramsId} />
       </>
    )
 }
--- frontend/src/widgets/dashboard/BudgetChart.tsx
+++ frontend/src/widgets/dashboard/BudgetChart.tsx
@@ -1,21 +1,34 @@
-import { type FC } from 'react';
+import { useEffect, useState, type FC } from 'react';
 import { Pie, PieChart, ResponsiveContainer, Cell } from 'recharts';
+import ChartExports from '../../shared/context/ChartContext';
+import { type IChartItem } from '../../shared/types/charts';
 
-const pieData = [
-  { name: 'Group A', value: 400 },
-  { name: 'Group B', value: 300 },
-  { name: 'Group C', value: 300 },
-  { name: 'Group D', value: 200 },
-];
+const { useChartContext } = ChartExports;
 
-const BudgetChart: FC = () => {
+interface IBudgetChartProps {
+   chartId: number;
+};
+
+const BudgetChart: FC<IBudgetChartProps> = ({ chartId }) => {
+   const [chartData, setChartData] = useState<IChartItem[] | null>(null);
+
+   const { charts } = useChartContext();
+
+   useEffect(() => {      
+      setChartData(charts[chartId-1].data);
+   }, [chartId, charts]);
+
+   if (chartData === null) {
+      return <span>нет данных</span>
+   }
+
    return (
-      <div style={{ width: '100%', height: '300px' }}>
+      <div style={{ width: '100%', height: '400px' }}>
          <ResponsiveContainer width='100%' height='100%'>
             <PieChart>
-               <Pie data={pieData} cx='50%' cy='50%' outerRadius={80} fill='#8884d8' dataKey='value' label={({ name, percent }) => `${name}: ${(percent * 100).toFixed(0)}%`}> 
-                  {pieData.map((entry, index) => (
-                     <Cell key={`cell-${entry.name}`} />
+               <Pie data={chartData} cx='50%' cy='50%' outerRadius={80} fill='#8884d8' dataKey='amount' label={({ category, percent }) => `${category}: ${(percent ? percent * 100 : 0).toFixed(0)}%`}> 
+                  {chartData.map((entry, index) => (
+                     <Cell key={index} />
                   ))}
                </Pie>
             </PieChart>
