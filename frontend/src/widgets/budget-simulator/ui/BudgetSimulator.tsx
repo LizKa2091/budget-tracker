cc77a85135f3fa5ea6346f28abbd27af86acfc57
lizka <lizka4231@gmail.com>
2025-07-31 21:43:20 +0300

    feat(budget chart): implement budget simulator into budget chart

--- frontend/src/widgets/budget-simulator/ui/BudgetSimulator.tsx
+++ frontend/src/widgets/budget-simulator/ui/BudgetSimulator.tsx
@@ -2,14 +2,20 @@ import { type FC } from 'react';
 import { Flex } from 'antd';
 import SetGoalForm from './SetGoalForm';
 import WhatIfCutForm from './WhatIfCutForm';
+import type { IChartItem } from '../../../shared/types/charts';
+import type { IExpensesByCategories } from '../../../shared/types/expenses';
 
-const BudgetSimulator: FC = () => {
+interface IBudgetSimulatorProps {
+   chartData: IChartItem[] | IExpensesByCategories[]
+};
+
+const BudgetSimulator: FC<IBudgetSimulatorProps> = ({ chartData }) => {
    return (
       <Flex vertical align='center' gap='large'>
          <p>Получите совет от нейросети</p>
          <Flex vertical gap='large'>
-            <SetGoalForm />
-            <WhatIfCutForm />
+            <SetGoalForm chartData={chartData} />
+            <WhatIfCutForm chartData={chartData} />
          </Flex>
       </Flex>
    )
