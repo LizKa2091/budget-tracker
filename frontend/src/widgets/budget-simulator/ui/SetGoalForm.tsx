fe5da1dea6e4713017a49e2af373327da2aa373f
lizka <lizka4231@gmail.com>
2025-07-31 21:42:33 +0300

    feat: implement use ai advice into set goal form and what if cut form

--- frontend/src/widgets/budget-simulator/ui/SetGoalForm.tsx
+++ frontend/src/widgets/budget-simulator/ui/SetGoalForm.tsx
@@ -1,16 +1,33 @@
-import { Button, Flex, Form, Input } from 'antd';
-import { type FC } from 'react'
+import { Button, Flex, Form, InputNumber } from 'antd';
+import { useState, type FC } from 'react'
 import { Controller, useForm } from 'react-hook-form';
+import type { IChartItem } from '../../../shared/types/charts';
+import type { IExpensesByCategories } from '../../../shared/types/expenses';
+import { useAIAdvice } from '../model/useAIAdvice';
 
 interface IGoalFormData {
-   goalValue: number;
+   goalValue: string;
 };
 
-const SetGoalForm: FC = () => {
+interface ISetGoalFormProps {
+   chartData: IChartItem[] | IExpensesByCategories[]
+};
+
+const SetGoalForm: FC<ISetGoalFormProps> = ({ chartData }) => {
+   const [AIResponse, setAIResponse] = useState<string>('');
    const { handleSubmit, control, formState: { errors }, trigger } = useForm<IGoalFormData>();
 
-   const onSubmit = (data: IGoalFormData): void => {
-      console.log(data);
+   const { mutateAsync, error } = useAIAdvice();
+
+   const onSubmit = async (formData: IGoalFormData): Promise<void> => {
+      try {
+         const response = await mutateAsync({ promptType: 'setGoal', value: formData.goalValue, expenses: chartData });
+
+         setAIResponse(response.answer.replaceAll('*', ''));
+      }
+      catch (error) {
+         console.error(error);
+      }
    };
 
    const onFinish = async (): Promise<void> => {
@@ -25,9 +42,11 @@ const SetGoalForm: FC = () => {
          <Form onFinish={onFinish}>
             <Flex vertical align='center'>
                <Form.Item label='Введите сумму' required validateStatus={errors.goalValue ? 'error' : ''} help={errors.goalValue && 'Обязательное поле'}>
-                  <Controller name='goalValue' control={control} rules={{ required: true }} render={({ field }) => <Input {...field} />} />
+                  <Controller name='goalValue' control={control} rules={{ required: true }} render={({ field }) => <InputNumber {...field} />} />
                </Form.Item>
                <Button htmlType='submit' type='default'>Поставить цель</Button>
+               {AIResponse && <p>{AIResponse}</p>}
+               {error && <p>Ошибка: {error.message}</p>}
             </Flex>
          </Form>
       </Flex>
--- frontend/src/widgets/budget-simulator/ui/WhatIfCutForm.tsx
+++ frontend/src/widgets/budget-simulator/ui/WhatIfCutForm.tsx
@@ -1,16 +1,33 @@
 import { Button, Flex, Form, Input } from 'antd';
-import { type FC } from 'react';
+import { useState, type FC } from 'react';
 import { Controller, useForm } from 'react-hook-form';
+import { useAIAdvice } from '../model/useAIAdvice';
+import type { IChartItem } from '../../../shared/types/charts';
+import type { IExpensesByCategories } from '../../../shared/types/expenses';
 
 interface ICutFormData {
    value?: string;
 };
 
-const WhatIfCutForm: FC = () => {
+interface IWhatIfCutFormProps {
+   chartData: IChartItem[] | IExpensesByCategories[];
+};
+
+const WhatIfCutForm: FC<IWhatIfCutFormProps> = ({ chartData }) => {
+   const [AIResponse, setAIResponse] = useState<string>('');
    const { handleSubmit, control } = useForm<ICutFormData>();
 
-   const onSubmit = (data: ICutFormData): void => {
-      console.log(data);
+   const { mutateAsync, error } = useAIAdvice();
+
+   const onSubmit = async (formData: ICutFormData): Promise<void> => {
+      try {
+         const response = await mutateAsync({ promptType: 'cutSpendings', value: formData.value ?? 'в целом', expenses: chartData });
+
+         setAIResponse(response.answer.replaceAll('*', ''));
+      }
+      catch (error) {
+         console.error(error);
+      }
    };
 
    return (
@@ -22,6 +39,8 @@ const WhatIfCutForm: FC = () => {
                   <Controller name='value' control={control} render={({ field }) => <Input {...field} />} />
                </Form.Item>
                <Button htmlType='submit' type='default'>Представить</Button>
+               {AIResponse && <p>{AIResponse}</p>}
+               {error && <p>Ошибка: {error.message}</p>}
             </Flex>
          </Form>
       </Flex>
