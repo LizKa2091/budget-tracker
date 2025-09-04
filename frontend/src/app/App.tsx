91968ba05eac9d2d2b6560bfc9276bef88bdbe61
lizka <lizka4231@gmail.com>
2025-09-04 21:25:53 +0300

    feat(app): add store provider, delete chart context

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,23 +1,23 @@
 import type { FC } from "react";
 import Router from "./Router";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import ChartExports from "../shared/context/ChartContext";
 import ThemeExports from "../shared/context/ThemeContext";
+import { Provider } from "react-redux";
+import { store } from "../features/chart-store";
 
-const { ChartContextProvider } = ChartExports;
 const { ThemeProvider } = ThemeExports;
 
 const queryClient = new QueryClient();
 
 const App: FC = () => {
    return (
-      <QueryClientProvider client={queryClient}>
-         <ThemeProvider>
-            <ChartContextProvider>
+      <Provider store={store}>
+         <QueryClientProvider client={queryClient}>
+            <ThemeProvider>
                <Router />
-            </ChartContextProvider>
-         </ThemeProvider>
-      </QueryClientProvider>
+            </ThemeProvider>
+         </QueryClientProvider>
+      </Provider>
    )
 }
 
