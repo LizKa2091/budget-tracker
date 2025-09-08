60142fad8c8807a9a4b5705a16b91c82d3437751
lizka <lizka4231@gmail.com>
2025-09-08 21:02:06 +0300

    feat(auth): add auth switcher component

---
+++ frontend/src/features/auth/ui/AuthSwitcher/AuthSwitcher.tsx
@@ -0,0 +1,46 @@
+import { Button, Flex } from 'antd';
+import { useState, type FC } from 'react'
+import LoginForm from '../LoginForm/LoginForm';
+import RegisterForm from '../RegisterForm/RegisterForm';
+import ForgotPassForm from '../ForgotPassForm/ForgotPassForm';
+import styles from './AuthSwitcher.module.scss';
+
+type AuthMode = 'login' | 'register' | 'forgot';
+
+const AuthSwitcher: FC = () => {
+   const [authMode, setAuthMode] = useState<AuthMode>('login');
+
+   const authForm = () => {
+      switch (authMode) {
+         case 'login':
+            return <LoginForm />;
+         case 'register':
+            return <RegisterForm />;
+         case 'forgot':
+            return <ForgotPassForm />;
+         default:
+            return null;
+      }
+   }
+
+   return (
+      <Flex vertical gap='large'>
+         {authForm()}
+
+         {authMode === 'login' &&
+            <Flex gap='middle'>
+               <Button onClick={() => setAuthMode('register')} className={styles.linkButton}>Нет аккаунта? Зарегистрироваться</Button>
+               <Button onClick={() => setAuthMode('forgot')} className={styles.linkButton}>Восстановить пароль</Button>
+            </Flex>
+         }
+         {authMode === 'register' &&
+            <Button onClick={() => setAuthMode('login')} className={styles.linkButton}>Уже есть аккаунт? Войти</Button>
+         }
+         {authMode === 'forgot' &&
+            <Button onClick={() => setAuthMode('login')} className={styles.linkButton}>Вернуться ко входу в аккаунт</Button>
+         }
+      </Flex>
+   )
+}
+
+export default AuthSwitcher
