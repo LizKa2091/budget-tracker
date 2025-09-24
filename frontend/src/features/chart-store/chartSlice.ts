f2f77893a57c2d80fdee44710cee3def7a82a494
lizka <lizka4231@gmail.com>
2025-09-25 00:38:30 +0300

    fix(chart-store): fix chart slice

--- frontend/src/features/chart-store/chartSlice.ts
+++ frontend/src/features/chart-store/chartSlice.ts
@@ -1,28 +1,37 @@
-import { createAsyncThunk, createSlice, type PayloadAction } from "@reduxjs/toolkit";
-import { categorizeExpenses } from "../../shared/lib/categorizeExpenses";
+import {
+   createAsyncThunk,
+   createSlice,
+   type PayloadAction,
+} from '@reduxjs/toolkit';
+import { categorizeExpenses } from '../../shared/lib/categorizeExpenses';
 import type { IChartSlot } from '../../shared/types/charts';
-import { categoryKeywords } from "../../shared/lib/categoryKeywords";
+import { categoryKeywords } from '../../shared/lib/categoryKeywords';
 import type { RootState } from './index';
-import { saveChartsToStorage } from "../../shared/lib/chartsLocalStorage";
-import type { IAddChartThunkArgs, IAddChartThunkResponse, IAddExpensePayload } from "./chartStoreTypes";
+import { saveChartsToStorage } from '../../shared/lib/chartsLocalStorage';
+import type {
+   IAddChartThunkArgs,
+   IAddChartThunkResponse,
+   IAddExpensePayload,
+} from './chartStoreTypes';
 
 interface IChartState {
    charts: IChartSlot[];
    userCategories: Record<string, string[]>;
-};
+}
 
 const savedCharts = localStorage.getItem('charts');
 const savedCategories = localStorage.getItem('userCategories');
 
 const initialState: IChartState = {
-   charts: savedCharts ? JSON.parse(savedCharts)
+   charts: savedCharts
+      ? JSON.parse(savedCharts)
       : [
-         { id: 1, name: null, data: null },
-         { id: 2, name: null, data: null },
-         { id: 3, name: null, data: null },
-         { id: 4, name: null, data: null }
-      ],
-   userCategories: savedCategories ? JSON.parse(savedCategories) : {}
+           { id: 1, name: null, data: null },
+           { id: 2, name: null, data: null },
+           { id: 3, name: null, data: null },
+           { id: 4, name: null, data: null },
+        ],
+   userCategories: savedCategories ? JSON.parse(savedCategories) : {},
 };
 
 const chartSlice = createSlice({
@@ -33,8 +42,11 @@ const chartSlice = createSlice({
          const { slotId } = action.payload;
          if (slotId > 4 || slotId < 0) return;
 
-         state.charts = state.charts.map((slot) => slot.id === slotId ? 
-         { ...slotId, data: null, categorizedData: null, name: null } : slot);
+         state.charts = state.charts.map((slot) =>
+            slot.id === slotId
+               ? { ...slot, data: null, categorizedData: null, name: null }
+               : slot
+         );
 
          saveChartsToStorage(state);
       },
@@ -44,21 +56,31 @@ const chartSlice = createSlice({
 
          if (!chart || !chart.data) return;
 
-         const updatedChartData = [...chart.data, { date, title, amount, category }];
+         const updatedChartData = [
+            ...chart.data,
+            { date, title, amount, category },
+         ];
          chart.data = updatedChartData;
-         chart.categorizedData = categorizeExpenses(updatedChartData, state.userCategories);
+         chart.categorizedData = categorizeExpenses(
+            updatedChartData,
+            state.userCategories
+         );
 
          const newCategory = category.trim().toLowerCase();
-         
-         const isExistingCategory = Object.keys(categoryKeywords).some(category => category.toLowerCase() === newCategory);
-         const isUserCategory = Object.keys(state.userCategories).some(category => category.toLowerCase() === newCategory);
+
+         const isExistingCategory = Object.keys(categoryKeywords).some(
+            (category) => category.toLowerCase() === newCategory
+         );
+         const isUserCategory = Object.keys(state.userCategories).some(
+            (category) => category.toLowerCase() === newCategory
+         );
 
          if (!isExistingCategory && !isUserCategory) {
             state.userCategories[category] = [];
          }
 
          saveChartsToStorage(state);
-      }
+      },
    },
    extraReducers: (builder) => {
       builder.addCase(addChartThunk.fulfilled, (state, action) => {
@@ -68,34 +90,55 @@ const chartSlice = createSlice({
             state.charts = initialState.charts;
          }
 
-         const emptyDataIndex = state.charts.findIndex((slot) => slot.data === null);
+         const emptyDataIndex = state.charts.findIndex(
+            (slot) => slot.data === null
+         );
 
          if (emptyDataIndex !== -1) {
             state.charts = state.charts.map((slot, index) => {
-               return index === emptyDataIndex ? { ...slot, data: newSlotData, categorizedData, name: chartName } : slot
+               return index === emptyDataIndex
+                  ? {
+                       ...slot,
+                       data: newSlotData,
+                       categorizedData,
+                       name: chartName,
+                    }
+                  : slot;
             });
-         }
-         else {
-            const newChart = { id: state.charts[3]?.id ?? 4, data: newSlotData, categorizedData: categorizedData, name: chartName };
+         } else {
+            const newChart = {
+               id: state.charts[3]?.id ?? 4,
+               data: newSlotData,
+               categorizedData: categorizedData,
+               name: chartName,
+            };
 
             state.charts = [...state.charts.slice(1), newChart];
          }
 
          localStorage.setItem('charts', JSON.stringify(state.charts));
-         localStorage.setItem('userCategories', JSON.stringify(state.userCategories));
-      })
-   }  
+         localStorage.setItem(
+            'userCategories',
+            JSON.stringify(state.userCategories)
+         );
+      });
+   },
 });
 
-export const addChartThunk = createAsyncThunk<IAddChartThunkResponse, IAddChartThunkArgs, { state: RootState }>(
-   'charts/addChart',
-   async ({ newSlotData, chartName }, { getState }) => {
-      const state = getState();
-      const userCategories = state.charts.userCategories;
+export const addChartThunk = createAsyncThunk<
+   IAddChartThunkResponse,
+   IAddChartThunkArgs,
+   { state: RootState }
+>('charts/addChart', async ({ newSlotData, chartName }, { getState }) => {
+   const state = getState();
+   const userCategories = state.charts.userCategories;
 
-      return { newSlotData, chartName, categorizedData: categorizeExpenses(newSlotData, userCategories)};
-   }
-)
+   return {
+      newSlotData,
+      chartName,
+      categorizedData: categorizeExpenses(newSlotData, userCategories),
+   };
+});
 
 export const { removeChart, addExpense } = chartSlice.actions;
 export default chartSlice.reducer;
